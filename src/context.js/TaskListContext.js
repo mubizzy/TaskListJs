41a43922aa4cd47551f6eb5700e5d09e91d9458b
unknown <ajibolamubarak304@gmail.com>
2022-09-07 14:12:59 +0100

    latest

--- src/context.js/TaskListContext.js
+++ src/context.js/TaskListContext.js
@@ -3,6 +3,11 @@ import React, { createContext, useState } from "react";
 export const TaskListContext = createContext();
 
 const TaskListContextProvider = () => {
+  const[tasks,setTasks]=useState([
+    {task:"read a book",id:1}
+    {task:"wash the car",id:2}
+    {task:"Write some code",id:3}
+  ])
   return <TaskListContext.Provider />;
 };
 
