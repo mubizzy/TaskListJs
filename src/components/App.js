75e422e797812b67395b1ed831fadcc7bf229b13
unknown <ajibolamubarak304@gmail.com>
2022-09-10 16:06:27 +0100

    TaskForm

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import TaskListContextProvider from "../context.js/TaskListContext";
 import "../App.css";
 import TaskList from "./TaskList";
 import TaskForm from "./TaskForm";
+import Header from "./Header";
 const App = () => {
   return (
     <TaskListContextProvider>
       <div className="container">
         <div className="app-wrapper">
+          <Header />
           <div className="main">
             <TaskForm />
             <TaskList />
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -2,22 +2,20 @@ import React from "react";
 
 const TaskForm = () => {
   return (
-    <form onSubmit={handleSubmit} className="form">
+    <form className="form">
       <input
         type="text"
         placeholder="Add Task..."
-        value={title}
-        onChange={handleChange}
+        // value={title}
+        // onChange={handleChange}
         required
         className="task-input"
       />
       <div className="buttons">
         <button type="submit" className="btn add-task-btn">
-          {editItem ? "Edit Task" : "Add Task"}
+          Add Task
         </button>
-        <button className="btn clear-btn" onClick={clearList}>
-          Clear
-        </button>
+        <button className="btn clear-btn">Clear</button>
       </div>
     </form>
   );
