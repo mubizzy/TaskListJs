a842a4265df3ef7f6d870758aecf0c2822c8e74b
unknown <ajibolamubarak304@gmail.com>
2022-09-11 09:27:57 +0100

    update

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -7,12 +7,14 @@ const TaskList = () => {
 
   return (
     <div>
-      <ul className="list">
-        {tasks.map((task) => {
-          return <Task task={task} key={task.id} />;
-        })}
-      </ul>
-      ) : (<div className="no-tasks">No Tasks</div>
+      {tasks.length ? (
+        <ul className="list">
+          {tasks.map((task) => {
+            return <Task task={task} key={task.id} />;
+          })}
+        </ul>
+      ) : (
+        <div className="no-tasks">No Tasks</div>
       )}
     </div>
   );
